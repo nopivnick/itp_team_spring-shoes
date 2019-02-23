43628aa86bfaf2af3c036ec731e2a8a39a93667f
nopivnick <nopivnick@nopivnick.com>
2019-02-23 18:11:49 -0500

    Refactor comments and consol messages

--- arduino/spring-shoes/spring-shoes.ino
+++ arduino/spring-shoes/spring-shoes.ino
@@ -21,26 +21,26 @@
 
 */
 
-int fsrAnalogPinBall = 1;      // FSR is connected to analog pin 1
-int fsrAnalogPinHeel = 2;      // FSR is connected to analog pin 2
+int fsrAnalogPinBall = 1;     // FSR is connected to analog pin 1
+int fsrAnalogPinHeel = 2;     // FSR is connected to analog pin 2
 
 int ledDigitalPinBall = 6;    // connect Blue LED to pin 6
 int ledDigitalPinHeel = 7;    // connect Red LED to pin 7
 
-int fsrReadingBall;            // the analog reading from the FSR sensor under the ball of the foot
-int fsrReadingHeel;            // the analog reading from the FSR sensor under the heel of the foot
+int fsrReadingBall;           // the analog reading from the FSR sensor under the ball of the foot
+int fsrReadingHeel;           // the analog reading from the FSR sensor under the heel of the foot
 
-int lastBallState = LOW;       // previous state of FSR sensor under the ball of the foot
-int lastHeelState = LOW;       // previous state of FSR sensor under the ball of the foot
+int lastBallState = LOW;      // previous state of FSR sensor under the ball of the foot
+int lastHeelState = LOW;      // previous state of FSR sensor under the ball of the foot
 
-int ballThreshold = 925;       // threshold sensor value used to detect Ball strike
-int heelThreshold = 700;       // threshold sensor value used to detect Heel strike
+int ballThreshold = 925;      // threshold sensor value used to detect Ball strike
+int heelThreshold = 700;      // threshold sensor value used to detect Heel strike
 
 int ballStrikeCount = 0;
 int heelStrikeCount = 0;
 
 void setup(void) {
-  Serial.begin(9600);          // We'll send debugging information via the Serial monitor
+  Serial.begin(9600);          // send debugging information to Serial Monitor
   pinMode(ledDigitalPinBall, OUTPUT);
   pinMode(ledDigitalPinHeel, OUTPUT);
 }
@@ -48,10 +48,10 @@ void setup(void) {
 void loop(void) {
   fsrReadingBall = analogRead(fsrAnalogPinBall);
   fsrReadingHeel = analogRead(fsrAnalogPinHeel);
-  Serial.print("FSR 1 reading = ");
+  Serial.print("BALL of foot = ");
   Serial.print(fsrReadingBall);
   Serial.print(" | ");
-  Serial.print("FSR 2 reading = ");
+  Serial.print("HEEL of foot = ");
   Serial.println(fsrReadingHeel);
 
   // read FSR sensor under Ball of foot
@@ -61,7 +61,7 @@ void loop(void) {
     // check that the previous value was below the threshold:
     if (lastBallState < ballThreshold) {
       // print to serial monitor when FSR sensor under Ball of foot passes the threshold
-      Serial.println("Ball sensor ABOVE the threshold");
+      Serial.println("BALL sensor ABOVE threshold");
       digitalWrite(ledDigitalPinBall, HIGH);
     }
   }
@@ -77,7 +77,7 @@ void loop(void) {
     // check that the previous value was below the threshold:
     if (lastHeelState < heelThreshold) {
       // print to serial monitor when FSR sensor under Heel of foot passes the threshold
-      Serial.println("Heel sensor ABOVE the threshold");
+      Serial.println("HEEL sensor ABOVE threshold");
       digitalWrite(ledDigitalPinHeel, HIGH);
     }
   }
