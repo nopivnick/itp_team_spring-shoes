e86617c84bf1d6dcde2627ea1b6a45b36a6602f4
Idit Barak <iditbarak@gmail.com>
2019-02-23 17:23:08 -0500

    Add new p5 sketch

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,7 +1,33 @@
-function setup() {
-  createCanvas(400, 400);
+
+var song;
+
+var val
+
+function preload(){
+  song = loadSound('assets/happy.mp3');
+
+  
 }
 
-function draw() {
-  background(220);
+
+function setup() {
+  createCanvas(200,200);
+ 
 }
+function draw() {
+  background(255);
+}
+
+function keyTyped() {
+  if (key === 'a') {
+      
+     song.play();
+ 
+    }
+  else if (key === 'b') {
+
+    song.stop();
+  }
+}
+
+
