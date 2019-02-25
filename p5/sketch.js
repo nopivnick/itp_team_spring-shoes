350af9d7de9879535922b07c4018c762407d0fae
nopivnick <nopivnick@nopivnick.com>
2019-02-24 20:02:17 -0500

    Refactor code for style

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,33 +1,22 @@
+let song;
+let val
 
-var song;
-
-var val
-
-function preload(){
+function preload() {
   song = loadSound('assets/happy.mp3');
-
-  
 }
-
 
 function setup() {
-  createCanvas(200,200);
- 
+  createCanvas(200, 200);
 }
+
 function draw() {
   background(255);
 }
 
 function keyTyped() {
   if (key === 'a') {
-      
-     song.play();
- 
-    }
-  else if (key === 'b') {
-
+    song.play();
+  } else if (key === 'b') {
     song.stop();
   }
 }
-
-
