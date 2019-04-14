92d1ae9d9185ad11aa227755de853eda5ebe9ada
nopivnick <nopivnick@nopivnick.com>
2019-04-13 20:52:13 -0400

    Add BLE example code to spring-shoes/sketch.js

--- p5/spring-shoes/sketch.js
+++ p5/spring-shoes/sketch.js
@@ -1,36 +1,108 @@
 /*
+  Spring Shoes
+  Idit Barak and Noah Pivnick
+  2189.00 Intro to Wearables (Jingwen Zhu)
+
+  This sketch borrows code from the following examples:
+
+   - BLE start notification example on Jingwen Zhu's ITP Intro to Wearables github repo
+     https://github.com/ZhuJingwen/intro-to-wearables-2019/tree/1b629b750c70ed50bbc4415cd270ffeea3e7c945/Week%207%20Examples/Week7_Example_BLE_startNotification
+
+*/
+
+let skipSong;
+let val
+
+/*
+   Bluetooth LE stuff
+*/
+
+// The serviceUuid must match the serviceUuid of the device you would like to connect
+const serviceUuid = "b6292c11-911a-4a51-b7f2-43fe53e62a77";
+let skipStateCharacteristic;
+let skipState = 0;
+let shoeBLE;
+
+/*
    p5.serialcontrol stuff
 */
 
 let serial; // declare a global variable for serial class
 let portName = '/dev/cu.usbmodem1411';
 
-let skipSong;
-let val
-
 function preload() {
   skipSong = loadSound('assets/skipSong.mp3');
 }
 
 function setup() {
+  // Create a p5ble class
+  shoeBLE = new p5ble();
+
   createCanvas(200, 200);
+  textSize(20);
+  textAlign(CENTER, CENTER);
 
   /*
-     p5.serialcontrol stuff
+     Bluetooth LE stuff
   */
 
-  serial = new p5.SerialPort("10.17.231.12"); // instantiate a new serial object
-  serial.on('list', printList); // list the available serial ports
-  serial.on('connected', serverConnected); // callback for connecting to the server
-  serial.on('open', portOpen); // callback for the port opening
-  serial.on('data', serialEvent); // callback for when new data arrives
-  serial.on('error', serialError); // callback for errors
-  serial.on('close', portClose); // callback for the port closing
-  serial.open(portName);
+  // Create a 'Connect and Start Notifications' button
+  const connectButton = createButton('Connect and Start Notifications')
+  connectButton.mousePressed(connectAndStartNotify);
+
+  // Create a 'Stop Notifications' button
+  const stopButton = createButton('Stop Notifications')
+  stopButton.mousePressed(stopNotifications);
 }
 
+function connectAndStartNotify() {
+  // Connect to a device by passing the service UUID
+  shoeBLE.connect(serviceUuid, gotCharacteristics);
+}
+
+// A function that will be called once got characteristics
+function gotCharacteristics(error, characteristics) {
+  if (error) console.log('error: ', error);
+  console.log('characteristics: ', characteristics);
+  skipStateCharacteristic = characteristics[0];
+  console.log(skipStateCharacteristic);
+  // Start notifications on the first characteristic by passing the characteristic
+  // And a callback function to handle notifications
+  shoeBLE.startNotifications(skipStateCharacteristic, handleNotifications);
+  // You can also pass in the dataType
+  // Options: 'unit8', 'uint16', 'uint32', 'int8', 'int16', 'int32', 'float32', 'float64', 'string'
+  // shoeBLE.startNotifications(skipStateCharacteristic, handleNotifications, 'string');
+}
+
+// A function that will be called once got characteristics
+function handleNotifications(data) {
+  console.log('data: ', data);
+  skipState = data;
+}
+
+// A function to stop notifications
+function stopNotifications() {
+  shoeBLE.stopNotifications(skipStateCharacteristic);
+}
+
+/*
+   p5.serialcontrol stuff
+*/
+
+serial = new p5.SerialPort("10.17.231.12"); // instantiate a new serial object
+serial.on('list', printList); // list the available serial ports
+serial.on('connected', serverConnected); // callback for connecting to the server
+serial.on('open', portOpen); // callback for the port opening
+serial.on('data', serialEvent); // callback for when new data arrives
+serial.on('error', serialError); // callback for errors
+serial.on('close', portClose); // callback for the port closing
+serial.open(portName);
+
+
 function draw() {
-  background(255);
+  background(250);
+  // Write value on the canvas
+  text(skipState, 100, 100);
 }
 
 // serial data from the Arduino stuff
